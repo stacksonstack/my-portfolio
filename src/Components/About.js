fe3e1c9801875772cf8992564ee60317d49747e3
stacksonstack <staceycarrillo44@gmail.com>
2021-02-25 16:45:50 -0500

    setup basic about page, need to answer questions

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,7 +1,11 @@
 function About(){
     return(
         <div>
-
+            <h1>About</h1>
+            <p> Html | Javascript | CSS | React | Ruby on Rails</p>
+            <p>Why do I love to code?</p>
+            <p>How did I start?</p>
+            <p>What are you doing now?</p>
         </div>
     )
 }
