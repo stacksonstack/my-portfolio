3272f5cd64aa5c0ccac4540b31e819fa182fdaa1
stacksonstack <staceycarrillo44@gmail.com>
2021-02-25 16:42:05 -0500

    updated readme, started navBard and About page

---
+++ src/Components/NavBar.js
@@ -0,0 +1,15 @@
+function NavBar() {
+    return (
+      <div >
+          <ul>
+              <li>Home</li>
+              <li>About</li>
+              <li>Projects</li>
+              <li>Blogs</li>
+              <li>Contact</li>
+          </ul>
+      </div>
+    );
+  }
+  
+  export default NavBar;
