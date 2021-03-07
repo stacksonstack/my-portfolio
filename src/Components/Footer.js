36b80963d1938f40264b22782fa8638e93efc860
stacksonstack <staceycarrillo44@gmail.com>
2021-03-07 10:16:02 -0500

    added floating and pushed animation to home page

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -2,7 +2,7 @@ import Icons from './Icons'
 function Footer(){
     return(
         <div className="footer">
-            <Icons />
+            
             <p id="footer-name">Stacey Carrillo &#169; 2020</p>
         </div>
     )
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -5,19 +5,21 @@ function Home(props) {
   const history = useHistory();
   const navigateTo = () => history.push("/about");
   return (
-      
     <div className="home">
     <div id="home-name">
     <h1 className="title-name-text">Stacey Carrillo</h1>
   </div>
-      <p>
+  <div id="home-text">
+  <p>
         I'm a NYC based full-stack developer focusing on creating clean & user
         friendly experiences
       </p>
       <button id="home-btn" onClick={navigateTo}>Check out my work &#x21E8; </button>
-      <Icons />
-    </div>
-    
+      
+      <div id="home-icons"> <Icons /></div>
+      </div>
+      
+      </div>
   );
 }
 export default Home;
