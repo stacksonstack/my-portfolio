a1c22d28c28333ceb896d018e96d2524aab242ba
stacksonstack <staceycarrillo44@gmail.com>
2021-03-22 19:21:27 -0400

    added blog

--- src/AppData/Blogs.js
+++ src/AppData/Blogs.js
@@ -2,13 +2,19 @@ export default function Blogs() {
   return [
     {
       title:
+        "NPM vs Yarn",
+      link:
+        "https://staceycarrillo.medium.com/npm-vs-yarn-5454ffbf739f",
+      picture: "/ProjectImages/blogNPM.png",
+      date: "March 22, 2021",
+    },
+    {
+      title:
         "Highlight The Active Navigation Bar Link Using <NavLink> in React",
       link:
         "https://staceycarrillo.medium.com/highlight-the-active-navigation-bar-link-using-navlink-in-react-d44f5d8bf997",
       picture: "/ProjectImages/blogNavBar.png",
       date: "March 12, 2021",
-      blurb:
-        "As I worked to create my portfolio website I had a list of UI goals I wanted to implement into my website. One of those goals was to highlight the navigation link I was currently on to a different color to let a user know what page they’re currently on.",
     },
     {
       title: "The CSS Transform Property",
@@ -16,8 +22,6 @@ export default function Blogs() {
         "https://staceycarrillo.medium.com/the-css-transform-property-1f0323a2b232",
       picture: "/ProjectImages/blogTransform.png",
       date: "Dec 3, 2020",
-      blurb:
-        "The transform: property is a great tool. It can help to create very cool animations and gives you, the developer the ability to rotate, scale, move, skew, etc., elements. In this blog, we’ll learn about the most general transform: property values out of the twenty-four that currently exist.",
     },
     {
       title: "Learn Animation Properties That Bring CSS To Life!",
@@ -25,8 +29,6 @@ export default function Blogs() {
         "https://staceycarrillo.medium.com/learn-animation-properties-that-bring-css-to-life-856df6af70a",
       picture: "/ProjectImages/blogAnimation.png",
       date: "Nov 12, 2020",
-      blurb:
-        "There are several different ways to animate an element using CSS. In this blog, we’ll cover a few different ways to animate background colors and images using linear animation, animation with steps, and @keyframes to complete our animation.",
     },
     {
       title:
@@ -35,8 +37,6 @@ export default function Blogs() {
         "https://staceycarrillo.medium.com/how-to-use-grid-area-and-grid-template-columns-to-create-a-responsive-website-f0c2cded4c8e",
       picture: "/ProjectImages/blogGrid.png",
       date: "Oct 21, 2020",
-      blurb:
-        "Learning to use grid can be a headache when you’re first starting out. Understanding a few concepts is key when creating your layout and knowing grid terminology is the first step on that journey.",
     },
     {
       title:
@@ -45,8 +45,6 @@ export default function Blogs() {
         "https://staceycarrillo.medium.com/5-design-principles-every-web-developer-should-know-to-create-an-effective-landing-page-and-website-7ea0d385c70f",
       picture: "/ProjectImages/blogDesign.png",
       date: "Sept 30, 2020",
-      blurb:
-        "Just like art, web design can be subjective. So how can we create landing pages that won’t make a user click away? Well there's no real formula that you can follow that’ll appeal to everyone but there are a few design principles that will help your page look clean and appealing.",
     },
   ];
 }
