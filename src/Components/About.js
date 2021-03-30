66eeb2f0e480eb86536ced98f8ee6cee8b112708
stacksonstack <staceycarrillo44@gmail.com>
2021-03-29 23:27:49 -0400

    added aboutMe image and text need to edit animation keyframes to account for 81 secs

--- src/Components/About.js
+++ src/Components/About.js
@@ -8,13 +8,25 @@ function About() {
         CSS
       </p>
       <div className="story">
-        <div id="story-image">
+        <div id="story-container">
           <img
-            id="laguardia"
-            src="/ProjectImages/laGuardia.png"
+            id="story-image"
+            src="/ProjectImages/aboutMe2.png"
             alt="school  building with laGuardia logo on the side"
           />
           <p id="story-text">
+            Hello! My name is Stacey and I'm a 23 yr old, Mexican-American,
+            full-stack web developer. FUN FACT: I have 3 dogs, 1 cat, and a
+            turtle. If you want to learn more about my journey keep watching
+            this animation, otherwise feel free to explore my website or send me
+            an email by filling out the form in the contact page.
+          </p>
+          <img
+            id="story-image2"
+            src="/ProjectImages/laGuardia.png"
+            alt="city buildings with flatiron school logo"
+          />
+          <p id="story-text2">
             I first learned how to code during my time at LaGuardia Community
             College. I began by taking free courses on Code Academy to prepare
             for my programming classes. However, it was not until I attended my
@@ -23,11 +35,11 @@ function About() {
             2020.
           </p>
           <img
-            id="story-image2"
+            id="story-image3"
             src="/ProjectImages/flatironSchool.png"
-            alt="city buildings with flatiron school logo"
+            alt="students graduating"
           />
-          <p id="story-text2">
+          <p id="story-text3">
             A month later, I attended Flatiron School to improve upon my
             programming skills. I worked on labs and built full-stack projects
             with talented software engineering students. I adopted the title
@@ -37,18 +49,18 @@ function About() {
             January 2020.
           </p>
           <img
-            id="story-image3"
+            id="story-image4"
             src="/ProjectImages/graduation.png"
             alt="students graduating"
           />
-          <p id="story-text3">
-            I'm currently working to improve my skills as a Software Engineer by
-            practicing algorithms on websites like Leet Code & Code Wars. I've
-            also begun to refactor my passion projects which feature a Beatles
-            fan site written in React. I'm also always working to improve my
-            portfolio website and make it 100% mobile friendly. My next goal is to obtain my first position as a
-            front-end developer and work in a team of people who share my
-            passion for building beautiful, user-friendly applications!{" "}
+          <p id="story-text4">
+            A month later, I attended Flatiron School to improve upon my
+            programming skills. I worked on labs and built full-stack projects
+            with talented software engineering students. I adopted the title
+            CSStacey as I helped many in my cohort learn how to write modern
+            CSS. Collaborating with my peers enabled me to solidify CSS concepts
+            such as Flexbox, Grid, Animations, and many more. I graduated in
+            January 2020.
           </p>
         </div>
       </div>
