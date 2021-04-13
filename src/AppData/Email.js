fa2ffdb3213ad5738785e710ca0b0d6bdbdcbbc9
stacksonstack <staceycarrillo44@gmail.com>
2021-04-12 21:12:17 -0400

    fixed errors to deploy on netlify

--- src/AppData/Email.js
+++ src/AppData/Email.js
@@ -1,4 +1,6 @@
-export default{
+const Email = {
     USER_ID: 'user_sKu1FISwj1sPD9vFSy1Wm',
     TEMPLATE_ID: 'personal-website'
 }
+
+export default Email
--- src/Components/CarouselWrapper.js
+++ src/Components/CarouselWrapper.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { Carousel } from "react-responsive-carousel";
-import styles from "react-responsive-carousel/lib/styles/carousel.min.css";
+// import styles from "react-responsive-carousel/lib/styles/carousel.min.css";
 
 class CarouselWrapper extends Component {
   render() {
